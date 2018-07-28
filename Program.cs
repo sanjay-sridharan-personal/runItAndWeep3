b45d257e8f2e6da20a84454898fb9641140d56c0
Sanjay Sridharan <sanjay.sridharan@alumni.utexas.net>
2018-07-28 07:02:12 +0530

    Set up project to parse expression from left to right starting with the leftmost operand, however, it is not yet implemented.

--- Program.cs
+++ Program.cs
@@ -2,100 +2,106 @@
 
 class RunMain
 {
+    // This program evaluates an arithmetic expression entered by the user
     static void Main()
     {
-        Console.WriteLine("This program adds multiple numbers together, however," +
-            " if you make a mistake typing you will have to start all over:");
+        Console.WriteLine("This program adds multiple numbers together, however, " +
+                          "if you make a mistake typing you cannot backspace:");
         var expression = ReadExpression();
         double value = EvaluateExpression(expression);
         Console.WriteLine($" {value}");
-        Console.ReadKey();
     }
 
+    // @returns This function returns the expression entered by the user as a string
+    //          filtering out all whitespace as it is irrelevant
     static string ReadExpression()
     {
-        // TODO(SS): Absorb any enter keys pressed after any operand
-        var input = string.Empty;
-        do
-        {
-            char keyPressed = Console.ReadKey().KeyChar;
-            if ('=' == keyPressed)
-                break;
-            input += keyPressed;
-        }
-        while (true);
-
-        // EvaluateExpression() expects an expression without spaces
         var expression = string.Empty;
-        foreach (char character in input)
-            if (character != ' ')
-                expression += character;
+        var keyPressed = Console.ReadKey();
+        char character = keyPressed.KeyChar;
+        while(character != '=')
+        {
+            switch(character)
+            {
+                case ' ': // Filter out all whitespace from expression
+                case '\t':
+                case '\n':
+                case '\r':
+                    break;
+                default:
+                    expression += character;
+                    break;
+            }
+
+            character = Console.ReadKey().KeyChar;
+        }
 
         return expression;
     }
 
+    // @returns This function returns the result of the user's arithmetic expression
+    // @param expression The arithmetic expression. Assumption: expression contains
+    //        NO whitespace or parenthesis like ( or )
     static double EvaluateExpression(string expression)
     {
-        // Expression is of the form N0+N1+N2+…+Nm, where there are NO SPACES at all, and you can replace one or more
-        // plus operators with minus operator. Assumption:
-        // 1. While negative numbers must be preceded by '-', positive numbers must NOT be preceded by '+'
-        // 2. Operands cannot be entered in exponential notation, however, decimal points are acceptable
-        return EvaluateSubtrahends(expression);
+        double[] operandArray = ParseOperands(expression);
+
+        // Assumption: To easily test this program assume the user wants only to
+        // add together numbers
+        return AddOperands(operandArray);
     }
 
-    static double ConvertOperandToNumber(string input)
+    // @returns The function takes the arithmetic expression and returns an array
+    //          of its operands (numbers, as opposed to operators which use the
+    //          numbers as input for, say, addition). Array order matches the
+    //          operands encountered when reading expression from left to right
+    // @param expression The arithmetic expression. Assumption: expression contains
+    //        NO whitespace or parenthesis like ( or )
+    static double[] ParseOperands(string expression)
     {
-        double numberInput = 0;
-        bool isValidDouble = Double.TryParse(input, out numberInput);
+        string operand = ReadLeftmostOperandAsString(expression);
+        var operandArray = new double[1];
+        operandArray[0] = ConvertOperandToDouble(operand);
+        return operandArray;
+    }
+
+    // @returns This function parses the passed-in expression and returns a truncated
+    //          version of it that contains just the leftmost operand. We expect
+    //          the first character of the expression to be the first character
+    //          of the leftmost operand also. Also, as expression does not contain
+    //          whitespace, we expect the character that follows the leftmost operand
+    //          to be an operator
+    static string ReadLeftmostOperandAsString(string expression)
+    {
+        return null;
+    }
+
+    // @returns This function takes a string and attempts to convert it to a double.
+    //          If successful, this double is returned, otherwise the program exits
+    // @param operandString The prospective operand to be converted to double
+    static double ConvertOperandToDouble(string operandString)
+    {
+        double operandDouble = 0;
+        bool isValidDouble = Double.TryParse(operandString, out operandDouble);
         if (!isValidDouble)
         {
-            Console.WriteLine($"\n\"{input}\" cannot be used as an operand, therefore, this program has exited.");
+            Console.WriteLine($"\n\"{operandString}\" is not a valid number, therefore," +
+                              " this program has exited.");
             const int userError = 0;
             Environment.Exit(userError);
         }
 
-        return numberInput;
+        return operandDouble;
     }
 
-    static double EvaluateAddends(string expression)
+    // @returns This function returns the sum of the operands in the array
+    // @param operandArray The list of operands to be added together
+    static double AddOperands(double[] operandArray)
     {
-        // Assumption: Plus characters are always operators
+        double sum = 0;
+        foreach (double operand in operandArray)
+            sum += operand;
 
-        // If expression is "-1+2+-3", split it on the pluses to produce an array like so ["-1", "2", "-3"]
-        var stringArray = expression.Split('+');
-
-        // Produce an addend array by converting each string element to a number
-        int arrayLength = stringArray.Length;
-        var addendArray = new double[arrayLength];
-        for (int index = 0; index < arrayLength; ++index)
-            addendArray[index] = EvaluateSubtrahends(stringArray[index]);
-
-        // Sum up addends
-        double result = 0;
-        foreach (double addend in addendArray)
-            result += addend;
-
-        return result;
-    }
-
-    static double EvaluateSubtrahends(string expression)
-    {
-        // Assumption: Negative characters are always operators
-
-        // If expression is "1-2", split it on the minuses to produce an array like so ["1", "2"]
-        var stringArray = expression.Split('-');
-
-        // Produce a subtrahend array by converting each string element to a number
-        int arrayLength = stringArray.Length;
-        var subtrahendArray = new double[arrayLength];
-        for (int index = 0; index < arrayLength; ++index)
-            subtrahendArray[index] = ConvertOperandToNumber(stringArray[index]);
-
-        // Initialize result to the minuend from which to subtract subtrahends
-        double result = subtrahendArray[0];
-        for (int index = 1; index < arrayLength; ++index)
-            result -= subtrahendArray[index];
-
-        return result;
+        return sum;
     }
 }
