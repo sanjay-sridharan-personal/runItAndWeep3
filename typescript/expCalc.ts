515876d5a60e8efac4b744d34b9fda31c0ca2e31
Emil Fred <emilfred@gmail.com>
2018-10-29 16:22:15 -0500

    refactor to add typescript proj(not working version)

---
+++ typescript/expCalc.ts
@@ -0,0 +1,33 @@
+function Add(x,y)
+{
+	return x+y;
+}
+
+function Subtract(x,y)
+{
+	return x-y;
+}
+
+function Multiply(x,y)
+{
+	return x*y;
+}
+
+function TryDivide(x,y)
+{
+	if(x > y)
+		return x/y;
+	else
+		return 0;
+}
+
+function calcMain(stringExp)
+{
+	var modExp = stringExp.replace(/[^0-9/+/-/*//]+/g,'');
+	var splitExp = modExp.split(/\D/);
+	return "SplitExp : " + splitExp[0] + " length :" +  splitExp.length;
+}
+
+let expString = prompt("Enter expression: ");
+
+document.body.innerHTML = calcMain(expString);
