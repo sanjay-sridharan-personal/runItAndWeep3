661e088f323d2308193b345620d76a358b015e0c
Emil Fred <emilfred@gmail.com>
2018-11-02 13:17:27 -0500

    working version of typescript

--- typescript/expCalc.js
+++ typescript/expCalc.js
@@ -13,10 +13,48 @@ function TryDivide(x, y) {
     else
         return 0;
 }
+function getOperands(cleanInputString) {
+    var numArray = [];
+    for (var i in cleanInputString) {
+        numArray[i] = parseFloat(cleanInputString[i]);
+    }
+    return numArray;
+}
+function calcValue(numArray, opArray) {
+    var total = numArray[0];
+    for (var i = 0; i < opArray.length; ++i) {
+        switch (opArray[i]) {
+            case "+":
+                total = Add(total, numArray[i + 1]);
+                break;
+            case "-":
+                total = Subtract(total, numArray[i + 1]);
+                break;
+            case "*":
+                total = Multiply(total, numArray[i + 1]);
+                break;
+            case "/":
+                total = TryDivide(total, numArray[i + 1]);
+                break;
+            case "":
+                break;
+        }
+    }
+    return total;
+}
 function calcMain(stringExp) {
-    var modExp = stringExp.replace(/[^0-9/+/-/*//]+/g, '');
-    var splitExp = modExp.split(/\D/);
-    return "SplitExp : " + splitExp[0] + " length :" + splitExp.length;
+    var operatorRegex = /[+-/*\/]/g;
+    var cleanRegex = /[^\+\-\*\/0-9]/g;
+    var modExp = stringExp.replace(cleanRegex, '');
+    var expResult = "";
+    var splitNum = modExp.split(operatorRegex);
+    var numArray = getOperands(splitNum);
+    var opArray = modExp.match(operatorRegex);
+    if (numArray.length = opArray.length + 1)
+        expResult = calcValue(numArray, opArray);
+    else
+        expResult = "Error in eval of expression";
+    return "stringExp :" + stringExp + "<br>" + "  modExp : " + modExp + "<br>" + "    numArray : " + numArray + "<br>" + "   opArray : " + opArray + "<br>" + "expResult = " + expResult;
 }
 var expString = prompt("Enter expression: ");
 document.body.innerHTML = calcMain(expString);
