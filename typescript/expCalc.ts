9c8eab707db7b37145b21e9fcb94aa10e81fcb5c
Emil Fred <emilfred@gmail.com>
2018-11-11 19:24:54 -0600

    Typescript update (#5)
    * refactor to add typescript proj(not working version)
    
    * moving README.md to correct location
    
    * working version of typescript
    
    * corrections
    
    * JS update
    
    * rebase from master

---
+++ typescript/expCalc.ts
@@ -0,0 +1,81 @@
+function Add(x,y)
+{
+	return x+y;
+}
+
+function Subtract(x,y)
+{
+	return x-y;
+}
+
+function Multiply(x,y)
+{
+	return x*y;
+}
+
+function TryDivide(x,y)
+{
+	if(x >= y)
+		return x/y;
+	else
+		return 0;
+}
+
+function getOperands(cleanInputString)
+{
+	let numArray: number[] = [];
+	for(let i in cleanInputString)
+	{
+		numArray[i] = parseFloat(cleanInputString[i]);
+	}
+	return numArray;
+}
+
+function calcValue (numArray, opArray)
+{
+	var total = numArray[0];
+	for(var i = 0 ; i < opArray.length; ++i)
+	{
+		switch (opArray[i])
+		{
+			case "+":
+				total = Add(total,numArray[i+1]);
+				break;
+			case "-":
+				total = Subtract(total,numArray[i+1]);
+				break;
+			case "*":
+				total = Multiply(total,numArray[i+1]);
+				break;								
+			case "/":
+				total = TryDivide(total,numArray[i+1]);
+				break;
+			case "":
+				break;								
+		}
+	}
+	return total;
+
+
+}
+
+function calcMain(stringExp)
+{
+	var operatorRegex = /[+-/*\/]/g;
+	var cleanRegex = /[^\+\-\*\/0-9]/g;
+	var modExp = stringExp.replace(cleanRegex,'');
+	var expResult = "";
+	var splitNum = modExp.split(operatorRegex);
+	var numArray = getOperands(splitNum);
+	var opArray = modExp.match(operatorRegex);
+	if (numArray.length = opArray.length+1)
+		 expResult = calcValue(numArray, opArray);
+	else
+		 expResult = "Error in eval of expression"
+
+	return "stringExp :" + stringExp + "<br>"+"  modExp : " + modExp + "<br>" + "    numArray : " + numArray + "<br>" + "   opArray : " + opArray + "<br>" + "expResult = " + expResult;
+}
+
+let expString = prompt("Enter expression: ");
+
+document.body.innerHTML = calcMain(expString);
